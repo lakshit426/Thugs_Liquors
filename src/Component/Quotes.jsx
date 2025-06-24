4df4e111a4edf19ccf5953fb1c4e7899e8007744
lakshit lakhani <lakhanilakshit426@gmail.com>
2025-06-24 09:06:22 +0530

    Thugs_Liquors

--- src/Component/Quotes.jsx
+++ src/Component/Quotes.jsx
@@ -3,7 +3,7 @@ import harry from '../Images/harry-removebg-preview.png'
 
 export default function Quotes() {
   return (
-    <div class="container rounded ">
+    <div class="container-fluid ">
     <div className="row">
       <div className="col-sm-6 bx p-3">
         <div>
@@ -14,7 +14,7 @@ export default function Quotes() {
         </div>
         <div class="blue">
           <h3>Alcohol: because no great story ever started with someone eating a salad.</h3>
-          <div class="fa fa-quote-right" id="right"></div>
+{/*           <div class="fa fa-quote-right" id="right"></div> */}
         
         </div>
       </div>
@@ -27,7 +27,7 @@ export default function Quotes() {
         </div>
         <div class="blue">
           <h3>A glass of wine a day keeps the doctor away, but a bottle a day brings the doctor to your house.</h3>
-          <div class="fa fa-quote-right " id="right"></div>
+{/*           <div class="fa fa-quote-right " id="right"></div> */}
          
         </div>
       </div>
