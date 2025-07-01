6f165b9b474c15c7ae6c838e0a7e75cbf2484d71
lakshit lakhani <lakhanilakshit426@gmail.com>
2025-07-01 19:05:09 +0530

    Thugs_Liquors

--- src/Component/Navigation.jsx
+++ src/Component/Navigation.jsx
@@ -1,40 +1,59 @@
-import React from 'react'
-// import Logo from '../Images/logo.png'
-import Thugs from '../Images/logo2.png'
-import { Outlet, Link } from 'react-router-dom'
-import Footer from './Footer'
+import React, { useRef } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import Footer from './Footer';
+import Thugs from '../Images/logo2.png';
+
 export default function Navigation({ size }) {
+  const collapseRef = useRef(null);
+
+  const handleNavLinkClick = () => {
+    if (collapseRef.current && window.innerWidth < 992) {
+      const bsCollapse = window.bootstrap.Collapse.getInstance(collapseRef.current) 
+        || new window.bootstrap.Collapse(collapseRef.current, { toggle: false });
+      bsCollapse.hide();
+    }
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div class="container-fluid">
-          <Link class="navbar-brand  text-light" to="/"><img src={Thugs} alt="" height="80px" width="145px" className='me-3' /></Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-            <span class="navbar-toggler-icon"></span>
+        <div className="container-fluid">
+          <Link className="navbar-brand text-light" to="/"><img src={Thugs} alt="" height="80px" width="145px" className='me-3' /></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
-            <ul class="navbar-nav">
-              <li class="nav-item mx-5">
-                <li><Link rel="stylesheet" to="/" class="nav-link text-light">Home</Link></li>
+          <div className="collapse navbar-collapse justify-content-center" id="collapsibleNavbar" ref={collapseRef}>
+            <ul className="navbar-nav">
+              <li className="nav-item mx-5">
+                <Link to="/" className="nav-link text-light" onClick={handleNavLinkClick}>Home</Link>
               </li>
-              <li class="nav-item mx-5">
-                <li><Link rel="stylesheet" to="/About" class="nav-link text-light">About</Link></li>
+              <li className="nav-item mx-5">
+                <Link to="/About" className="nav-link text-light" onClick={handleNavLinkClick}>About</Link>
               </li>
-              <li class="nav-item dropdown mx-5">
-                <a class="nav-link  text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Category</a>
-                <ul class="dropdown-menu">
-                  <li><Link class="dropdown-item" to="/whiskey">Whiskey</Link></li>
-                  <li><Link class="dropdown-item" to="/vodka">Vodka</Link></li>
-                  <li><Link class="dropdown-item" to="/beer">Beer</Link></li>
-                  <li><Link class="dropdown-item" to="/rum">Rum</Link></li>
-                  <li><Link class="dropdown-item" to="/cocktail">Cocktail</Link></li>
-                  <li><Link class="dropdown-item" to="/domestic">Domestic</Link></li>
+              <li className="nav-item dropdown mx-5">
+                <a className="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown">
+                  Category
+                </a>
+                <ul className="dropdown-menu">
+                  {["whiskey", "vodka", "beer", "rum", "cocktail", "domestic"].map((item) => (
+                    <li key={item}>
+                      <Link className="dropdown-item" to={`/${item}`} onClick={handleNavLinkClick}>
+                        {item.charAt(0).toUpperCase() + item.slice(1)}
+                      </Link>
+                    </li>
+                  ))}
                 </ul>
               </li>
-              <li class="nav-item mx-5">
-                <li><Link rel="stylesheet" to="/LoginPage" class="nav-link text-light">LoginPage</Link></li>
+              <li className="nav-item mx-5">
+                <Link to="/LoginPage" className="nav-link text-light" onClick={handleNavLinkClick}>LoginPage</Link>
               </li>
-              <Link to="/cart"><button className='btn btn-warning mycartbtn'>Cart <span className='badge bg-danger'>{size}</span></button></Link>
+              <li className="nav-item mx-5">
+                <Link to="/cart" onClick={handleNavLinkClick}>
+                  <button className='btn btn-warning mycartbtn'>
+                    Cart <span className='badge bg-danger'>{size}</span>
+                  </button>
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
@@ -42,5 +61,5 @@ export default function Navigation({ size }) {
       <Outlet />
       <Footer />
     </>
-  )
+  );
 }
